506c637420e8e674c33b631908bdade2ac4fb3bd
Oraka Wellington <orakabw@gmail.com>
2019-03-12 20:57:44 -0400

    new-codewars-submit

---
+++ codewar2/main.js
@@ -0,0 +1,8 @@
+//Complete the solution so that it reverses the string value passed into it.
+
+function solution(str){
+    var split = str.split("");
+    var reverse = split.reverse();
+    var join = reverse.join("");
+     return join;
+  }
